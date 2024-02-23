071898d4deb4ad317ed0deb5842b68e9963d9fb7
Harsh-Dorlekar <harshdorlekar@gmail.com>
2024-02-23 13:30:23 -0500

    Midterm Final Question Completed!!

---
+++ app/src/main/java/com/example/midterm/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.midterm
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val button = findViewById<Button>(R.id.b1)
+        button.setOnClickListener()
+        {
+            callActivity()
+        }
+    }
+
+    private fun callActivity()
+    {
+
+        val intent = Intent(this, MainActivity2::class.java).also{
+            startActivity(it)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/midterm/MainActivity2.kt
@@ -0,0 +1,17 @@
+package com.example.midterm
+
+
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+        val textView = findViewById<TextView>(R.id.t1).apply{
+            text="This is mid-term exam"
+        }
+    }
+}
